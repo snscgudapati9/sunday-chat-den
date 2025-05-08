aae461e40075b866f7c887bb2c5054e2f591904b
G.snsc <91589702+snscgudapati9@users.noreply.github.com>
2025-05-08 11:05:05 +0530

    sunday.py
    updated ports

--- sunday.py
+++ sunday.py
@@ -48,6 +48,5 @@ def load_tasks():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
